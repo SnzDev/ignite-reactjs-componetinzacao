cae1811294e91df306d2749848d4e3587283ef98
SnzDev <tms.working@gmail.com>
2022-04-25 22:44:34 -0300

    Created component to sidebar and content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,15 @@
-export function Content() {
-  // Complete aqui
+
+import { MovieCard } from './MovieCard';
+import '../styles/content.scss';
+
+export function Content({ movies }) {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,23 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import '../styles/sidebar.scss';
+
+
+export function SideBar({ genres, onClick, genId }) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClick(genre.id)}
+            selected={genId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>);
 }
